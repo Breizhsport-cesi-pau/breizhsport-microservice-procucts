1b73af6525fa663bca9d8a37898d1d2e198663b7
sacha TORTELLI <sachabasse@gmail.com>
2025-01-24 14:59:00 +0100

    test docker 4 + test

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const productRoutes = require('./src/routes/productRoutes');
+const variantRoutes = require('./src/routes/variantRoutes');
+const categorieRoutes = require('./src/routes/categorieRoute');
+const productsCategoriesRoutes = require('./src/routes/productsCategoriesRoutes');
+const pictureRoute = require('./src/routes/picturesRoute');
+
+app.use('/categories', categorieRoutes);
+app.use('/variants', variantRoutes);
+app.use('/products', productRoutes);
+app.use('/products-categories', productsCategoriesRoutes);
+app.use('/pictures',pictureRoute);
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,32 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./src/config/database');
-
-const app = express();
+const app = require("./app");
 const PORT = process.env.PORT;
-
-// Synchronisation de la base de données
-sequelize.sync().then(() => {
-    console.log('Base de données SQLite synchronisée');
-}).catch((error) => {
-    console.error('Erreur lors de la synchronisation de la base de données:', error);
-});
-
-app.use(express.json());
-
-const productRoutes = require('./src/routes/productRoutes');
-const variantRoutes = require('./src/routes/variantRoutes');
-const categorieRoutes = require('./src/routes/categorieRoute');
-const productsCategoriesRoutes = require('./src/routes/productsCategoriesRoutes');
-const pictureRoute = require('./src/routes/picturesRoute');
-
-app.use('/categories', categorieRoutes);
-app.use('/variants', variantRoutes);
-app.use('/products', productRoutes);
-app.use('/products-categories', productsCategoriesRoutes);
-app.use('/pictures',pictureRoute);
-
-
 
 sequelize.sync({ alter: true })
     .then(() => console.log('Base de données synchroniséeee'))
