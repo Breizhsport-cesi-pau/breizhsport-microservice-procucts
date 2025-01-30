4f243a08c8114e8ebe6e4fa6231d70a6be39ffa2
sacha TORTELLI <sachabasse@gmail.com>
2025-01-30 22:52:58 +0100

    ok normalement go

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const sequelize = require('./src/config/database');
 const app = require("./app");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3002;
 
 sequelize.sync({ alter: true })
     .then(() => console.log('Base de données synchroniséeee'))
-    .catch(err => console.error('Erreur lors de la synchronisation :', err));
+    
 
 app.listen(PORT, () => {
     console.log(`Service de gestion des produit sur http://localhost:${PORT}`);
 });
+
+module.exports = app;
